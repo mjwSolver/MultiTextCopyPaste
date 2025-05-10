1fff43b9b6b2342a259a7a66e808646ae5b7a5bb
mjwSolver <mjwiradinata@gmail.com>
2025-05-10 10:44:16 +0700

    Assembly Settings Fixed and New Setup Followed instructions to update the registry as well as update application in the properties option.

--- MultiTextCopyPaste/Properties/AssemblyInfo.cs
+++ MultiTextCopyPaste/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Security;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("adfade95-90d2-45c6-93ab-e76861f032e4")]
